6eb3e7adaf4153ffe74bf2b2b1a126727923fd4b
Andres Cuvertino <andreskuver@gmail.com>
2016-10-13 10:32:37 -0300

    v0.0.0

---
+++ ng-table.js
@@ -0,0 +1,41 @@
+(function(angular) {
+  'use strict';
+  angular.module("ngTable", [])
+
+  .directive('ngTable', function() {
+    return {
+      restrict: 'E',
+      scope: {
+        options: '=options',
+      },
+      template: 
+        '<table class="table table-striped table-bordered table-hover">'
+        + '<thead>'
+        + '<tr>'
+        + '<th data-ng-repeat="(key,value) in options.columns">{{value.title}}</th>'
+        + '</tr>'
+        + '</thead>'
+        + '<tbody>'
+        + '<tr data-ng-repeat="obj in data">'
+        + '<td ng-repeat="col in options.columns">{{obj[col.data]}}</td>'
+        + '</tr>'
+        + '</tbody></table>',
+      controller: function($scope, $element, $http){
+        const headers = [];
+        init();
+        function init() {
+          getData();
+        }
+
+        function getData() {
+          $http.get($scope.options.url)
+            .then((data) => { 
+              $scope.data = data.data.rows; 
+
+            });
+        }
+      },
+    }
+  });
+})(window.angular);;
+
